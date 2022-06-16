5de670a6e1591f52b76ccd03f4e96bd992d395bd
Niki <nikitheofilaktidou@gmail.com>
2022-06-16 14:06:01 +0300

    Implementing repositories

--- TaskManager/Repositories/SQLTodoTaskRepository.cs
+++ TaskManager/Repositories/SQLTodoTaskRepository.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskManager.IRepositories;
+using TaskManager.Models;
 
 namespace TaskManager.Repositories
 {
-    public class SQLTodoTaskRepository :ITodoTaskRepository
+    public class SQLTodoTaskRepository : ITodoTaskRepository
     {
+        public TodoTask Add(TodoTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoTask Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoTask> GetAllTodoTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoTask GetTodoTask(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoTask Update(TodoTask taskChanges)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
